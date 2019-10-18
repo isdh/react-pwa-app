b378b50e05cd17efb06224f1676f24dadc373a88
isdh <hideorseekyes+github@gmail.com>
2019-10-18 15:25:01 +0900

    Updates

---
+++ precache-manifest.2a20c12a9453f37fa99cb419856429c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7445b3307a2071a14100d6ec798c2d18",
+    "url": "/react-pwa-app/index.html"
+  },
+  {
+    "revision": "e30678d7b4a6a6473c3b",
+    "url": "/react-pwa-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8faf4634ecd5d5a9d568",
+    "url": "/react-pwa-app/static/js/2.cb367905.chunk.js"
+  },
+  {
+    "revision": "e30678d7b4a6a6473c3b",
+    "url": "/react-pwa-app/static/js/main.3ac12e7e.chunk.js"
+  },
+  {
+    "revision": "ebfefe598c8571bfb7b1",
+    "url": "/react-pwa-app/static/js/runtime-main.b5162dc5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-pwa-app/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/isdh/react-pwa-app/precache-manifest.2420f85422e6fe7209c06c974c8fd4fc.js"
+  "/react-pwa-app/precache-manifest.2a20c12a9453f37fa99cb419856429c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/isdh/react-pwa-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pwa-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
